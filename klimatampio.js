699b416f0338ef14b460092c6dfcba0b7ad65036
dajogzum <33661462+dajogzum@users.noreply.github.com>
2017-11-14 18:36:52 +0100

    Update klimatampio.js

--- klimatampio.js
+++ klimatampio.js
@@ -22,9 +22,9 @@ Module.register("klimatampio", {
 
 	instertData: function (){
 		var link = "http://"+this.config.ip+":8060/api/json/device";
-		var tempid = this.config.id1; 
-		var wilgid = this.config.id2;
-		var cisid = this.config.id3;
+		var tempid = this.config.id1-1; 
+		var wilgid = this.config.id2-1;
+		var cisid = this.config.id3-1;
 		var xhttp = new XMLHttpRequest();
 		var json = [];
   		setInterval(function (){
@@ -34,9 +34,9 @@ Module.register("klimatampio", {
   			xhttp.onreadystatechange = function() {
     			if (this.readyState == 4) {
       				json = JSON.parse(this.response).List;
-				var temp = (json.tempid.state*100)/100;
-				var wilg = json.wilgid.state;
-				var cis = json.cisid.state;
+				var temp = (json[tempid].state*100)/100;
+				var wilg = json[wilgid].state;
+				var cis = json[cisid].state;
       				document.getElementById("klimatampio").innerHTML = "Temperatura: "+temp+"&#186C</br>Wilgotność: "+wilg+"%</br>Ciśnienie: "+cis+" hPa";
     				}
   			}
